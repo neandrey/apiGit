93ec9e83060fce72c3196f92a71eef2c514d27be
ne_andrey <sorokinac@gmail.com>
2018-01-30 21:18:08 +0500

    apiGitHub

--- apiGitHub.py
+++ apiGitHub.py
@@ -2,6 +2,7 @@ import requests
 import pygal
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
+
 #создание вызовов API и сохранение ответов.
 url = 'https://api.github.com/search/repositories?q=language:python &sort=star'
 r = requests.get(url)
@@ -19,18 +20,18 @@ repo_dicts = response_dict['items']
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+
 #построение визуализации
 my_style = LS('#333366', base_style=LCS)
-chart = pygal.Bar(style = my_style, x_label_rotation = 45,
-                  show_legend = False)
+chart = pygal.Bar(style = my_style, x_label_rotation = 45, show_legend = False)
 chart.title = "Most-Starred Python Projects on GitHub"
 chart.x_labels = names
 
 chart.add('', stars)
 chart.render_to_file('python_repos.svg')
 
-#анализ первого репозитория
-#repo_dict = repo_dicts[0]
-#print("\nKeys: ", len(repo_dict))
-#for key in sorted(repo_dict.keys()):
-#    print(key)
+# анализ первого репозитория
+# repo_dict = repo_dicts[0]
+# print("\nKeys: ", len(repo_dict))
+# for key in sorted(repo_dict.keys()):
+#     print(key)
